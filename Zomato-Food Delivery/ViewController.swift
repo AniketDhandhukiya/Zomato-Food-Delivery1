902b3a20b1f992fe3945d89a55419a0aeb8980e8
AniketDhandhukiya <addhandhukiya2309@gmail.com>
2023-04-13 14:10:18 +0530

    co

--- Zomato-Food Delivery/ViewController.swift
+++ Zomato-Food Delivery/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mobileText: UITextField!
+    @IBOutlet weak var loginimage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loginimage.layer.cornerRadius = 45
+        loginimage.layer.masksToBounds = true
     }
 
 
